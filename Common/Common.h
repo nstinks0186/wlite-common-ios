d78c8fbb0804647824fbd72393c177d581e3908c
Pinuno Fuentes <nstinks0186@gmail.com>
2015-06-10 20:42:03 -0400

    Initial Commit

---
+++ Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Pinuno Fuentes on 6/10/15.
+//  Copyright (c) 2015 Wunderlite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
