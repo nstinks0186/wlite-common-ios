92d58108a35427f69adf8c4d63edb9e790759d39
Pinuno Fuentes <nstinks0186@gmail.com>
2015-06-19 15:59:56 -0400

    blah

--- CommonDemo/AppDelegate.swift
+++ CommonDemo/AppDelegate.swift
@@ -11,11 +11,11 @@ import Alamofire
 import Common
 
 struct App {
-    static let clientID = "71436ba1a9dc81c908f1"
-    static let clientSecret = "0974ff4a3fbd2403522a7608c5a227e2e06f8925cdab58fd7a34f8199dc8"
+    static let clientID = "**"
+    static let clientSecret = "**"
     // this is the callback url that is registered in wunderlist
     // that callback url should redirect to <app url scheme>://wlite?access_token=<the token>
-    static let callbackURL = "https://dl.dropboxusercontent.com/u/33491043/sites/wlite/success.html"
+    static let callbackURL = "**"
     static var accessToken : String?
 }
 
